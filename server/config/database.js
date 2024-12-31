3e7cd28f5a87e3cab63ce81b230a7710f7a65248
ritiktomar02 <ritiktomar0209@gmail.com>
2024-12-31 18:54:15 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,25 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const  MONGODB_URL  = "mongodb+srv://ritiktomar0209:fKnK8Vjmd3GoE47h@cluster0.xwgrg.mongodb.net/Studynotion";
+// MongoDB connection URL
+const MONGODB_URL =  "mongodb+srv://ritiktomar0209:fKnK8Vjmd3GoE47h@cluster0.xwgrg.mongodb.net/Studynotion";
 
+// Function to connect to the database
 exports.connect = () => {
-	mongoose
-		.connect(MONGODB_URL)
-		.then(console.log(`DB Connection Success`))
-		.catch((err) => {
-			console.log(`DB Connection Failed`);
-			console.log(err);
-			process.exit(1);
-		});
+    console.log("Attempting to connect to the database...");
+
+    mongoose
+        .connect(MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, // 30 seconds timeout for database server selection
+        })
+        .then(() => {
+            console.log("Database connected successfully");
+        })
+        .catch((err) => {
+            console.error("Database connection failed:");
+            console.error(err.message);
+            process.exit(1); // Exit the application if the connection fails
+        });
 };
