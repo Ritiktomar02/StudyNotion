0b19e78a164792f48e2c028941860e771af7c9ba
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2024-12-31 18:31:33 +0530

    url

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "*",
+		origin: "https://studynotion-frontend-eight-sigma.vercel.app" || "http://localhost:3000",
 		credentials: true,
 	})
 );
